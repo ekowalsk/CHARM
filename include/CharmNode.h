dab42b806f2d3b84416fce6865f8e18b7eafa448
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-08 16:06:42 +0100

    zaimplementowano metodę IntersectTidList

--- include/CharmNode.h
+++ include/CharmNode.h
@@ -1,8 +1,20 @@
-//
-// Created by ewa on 08.01.20.
-//
-
 #ifndef PROJEKT_CHARMNODE_H
 #define PROJEKT_CHARMNODE_H
 
+#include "Node.h"
+
+class CharmNode : public Node {
+public:
+    typedef std::list<int> tid_list;
+private:
+    tid_list * tidList;
+
+    static tid_list * copyTidList(tid_list * tidList);
+    void intersectTidList(tid_list * intersectSource);
+
+public:
+    CharmNode(CharmNode * parent, item_set * itemSet, tid_list * tidList);
+    ~CharmNode();
+
+};
 #endif //PROJEKT_CHARMNODE_H
--- src/CharmNode.cpp
+++ src/CharmNode.cpp
@@ -1,1 +1,31 @@
+#include "CharmNode.h"
 
+CharmNode::CharmNode(CharmNode * parent, item_set * itemSet, tid_list * tidList): Node(parent, itemSet){
+    this->tidList = copyTidList(tidList);
+}
+
+CharmNode::tid_list * CharmNode::copyTidList(tid_list * tidList){
+    auto tidListCopy = new tid_list();
+    for (auto &tid : *tidList)
+        tidListCopy->push_back(tid);
+    return tidListCopy;
+}
+
+void CharmNode::intersectTidList(tid_list * intersectSource){
+    tidList->sort();
+    intersectSource->sort();
+    auto intersectionTidList = new tid_list();
+    auto destIterator = tidList->begin();
+    for (auto &sourceTid : *intersectSource){
+        while (destIterator != tidList->end() && *destIterator < sourceTid)
+            destIterator++;
+        if (*destIterator == sourceTid)
+            intersectionTidList->push_back(sourceTid);
+    }
+    delete tidList;
+    tidList = intersectionTidList;
+}
+
+CharmNode::~CharmNode() {
+    delete tidList;
+}
