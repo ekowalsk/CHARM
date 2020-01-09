27423b65e7ad4c1ece2e3b3462b5dceeae25e07e
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-09 10:54:20 +0100

    dodano metody insertChild i removeChild

--- include/CharmNode.h
+++ include/CharmNode.h
@@ -1,13 +1,23 @@
 #ifndef PROJEKT_CHARMNODE_H
 #define PROJEKT_CHARMNODE_H
 
-#include "Node.h"
+#include <list>
+#include <map>
 
-class CharmNode : public Node {
+class CharmNode {
 public:
     typedef std::list<int> tid_list;
+    typedef std::list<int> item_set;
+    typedef std::multimap<int, CharmNode *>::iterator childIterator;
 private:
+    item_set * itemSet;
     tid_list * tidList;
+
+    CharmNode * parent;
+    std::multimap<int, CharmNode *> * children; // map of pairs <support, Node>
+
+    static item_set * copyItemSet(const item_set * source);
+    static item_set * unionItemSet(item_set * itemSet1, item_set * itemSet2);
 
     static tid_list * copyTidList(tid_list * tidList);
     static tid_list * intersectTidList(tid_list * tidList1, tid_list * tidList2);
@@ -16,8 +26,16 @@ public:
     CharmNode(CharmNode * parent, item_set * itemSet, tid_list * tidList);
     ~CharmNode();
 
-    static tid_list * intersectedTidList(CharmNode node1, CharmNode node2);
+    static item_set * unionItemSet(CharmNode * node1, CharmNode * node2);
+    static tid_list * intersectedTidList(const CharmNode& node1, const CharmNode& node2);
+
+    void setItemSet(item_set * itemSet);
     void setTidList(tid_list * tidList);
-    int countHash();
+
+    void insertChild(CharmNode * child);
+    void removeChild(childIterator childIt);
+
+    int getHash();
+    int getSupport();
 };
 #endif //PROJEKT_CHARMNODE_H
--- src/CharmNode.cpp
+++ src/CharmNode.cpp
@@ -1,7 +1,37 @@
 #include "CharmNode.h"
 
-CharmNode::CharmNode(CharmNode * parent, item_set * itemSet, tid_list * tidList): Node(parent, itemSet){
+CharmNode::CharmNode(CharmNode * parent, item_set * itemSet, tid_list * tidList){
+    this->itemSet = copyItemSet(itemSet);
     this->tidList = copyTidList(tidList);
+    this->parent = parent;
+    children = new std::multimap<int, CharmNode *>();
+}
+
+CharmNode::item_set * CharmNode::copyItemSet(const item_set * source){
+    auto itemSetCopy = new item_set();
+    for (auto &item : *source)
+        itemSetCopy->push_back(item);
+    return itemSetCopy;
+}
+
+CharmNode::item_set * CharmNode::unionItemSet(item_set * itemSet1, item_set * itemSet2){
+    itemSet1->sort();
+    itemSet2->sort();
+    auto * itemSetUnion = new item_set();
+    auto itemIterator = itemSet1->begin();
+    for (auto &item : *itemSet2){
+        while (itemIterator != itemSet1->end() && *itemIterator < item)
+            itemIterator++;
+        if (*itemIterator == item)
+            continue;
+        else
+            itemSetUnion->insert(itemIterator, item);
+    }
+    return itemSetUnion;
+}
+
+CharmNode::item_set * CharmNode::unionItemSet(CharmNode * node1, CharmNode * node2){
+    return unionItemSet(node1->itemSet, node2->itemSet);
 }
 
 CharmNode::tid_list * CharmNode::copyTidList(tid_list * tidList){
@@ -9,10 +39,6 @@ CharmNode::tid_list * CharmNode::copyTidList(tid_list * tidList){
     for (auto &tid : *tidList)
         tidListCopy->push_back(tid);
     return tidListCopy;
-}
-
-CharmNode::tid_list * CharmNode::intersectedTidList(CharmNode node1, CharmNode node2){
-    return intersectTidList(node1.tidList, node2.tidList);
 }
 
 CharmNode::tid_list * CharmNode::intersectTidList(tid_list * tidList1, tid_list * tidList2){
@@ -29,18 +55,46 @@ CharmNode::tid_list * CharmNode::intersectTidList(tid_list * tidList1, tid_list 
     return intersectionList;
 }
 
-void CharmNode::setTidList(tid_list * tidList){
-    delete this->tidList;
-    this->tidList = copyTidList(tidList);
+CharmNode::tid_list * CharmNode::intersectedTidList(const CharmNode& node1, const CharmNode& node2){
+    return intersectTidList(node1.tidList, node2.tidList);
 }
 
-int CharmNode::countHash(){
+void CharmNode::setItemSet(item_set * itSet){
+    delete this->itemSet;
+    this->itemSet = copyItemSet(itSet);
+}
+
+void CharmNode::setTidList(tid_list * tList){
+    delete this->tidList;
+    this->tidList = copyTidList(tList);
+}
+
+void CharmNode::insertChild(CharmNode * child){
+    int childSupport = child->getSupport();
+    children->insert({childSupport, child});
+}
+
+void CharmNode::removeChild(childIterator childIt){
+    children->erase(childIt);
+}
+
+int CharmNode::getHash(){
     int tidSum = 0;
     for (auto &tid : *tidList)
         tidSum += tid;
     return tidSum;
 }
 
+int CharmNode::getSupport(){
+    int support = 0;
+    for (auto &tid : *tidList)
+        support += 1;
+    return support;
+}
+
 CharmNode::~CharmNode() {
+    delete itemSet;
     delete tidList;
+    delete parent;
+    delete children;
 }
