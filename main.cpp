35b810f1eaa846a3829ea23647a7c3e2986877bc
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-14 22:59:22 +0100

    dodano przykład użycia DCharm

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 
 #include "Charm.h"
+#include "DCharm.h"
 
 int main() {
 CharmNode * node = new CharmNode(nullptr, nullptr, nullptr);
 node->insertChild(new CharmNode(node, new CharmNode::item_set ({1}), new CharmNode::tid_list ({2,4,5,6})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({2}), new CharmNode::tid_list ({1,3,5,6})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({3}), new CharmNode::tid_list ({1,3,4,5})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({4}), new CharmNode::tid_list ({1,2,3,4,5})));
 node->insertChild(new CharmNode(node, new CharmNode::item_set ({5}), new CharmNode::tid_list ({1,2,3,4,5,6})));
+node->insertChild(new CharmNode(node, new CharmNode::item_set ({2}), new CharmNode::tid_list ({1,3,5,6})));
+node->insertChild(new CharmNode(node, new CharmNode::item_set ({4}), new CharmNode::tid_list ({1,2,3,4,5})));
+node->insertChild(new CharmNode(node, new CharmNode::item_set ({3}), new CharmNode::tid_list ({1,3,4,5})));
 
 Charm charm = Charm();
 charm.charm(&node, 3);
 charm.printClosedItemsets();
+
+DCharmNode * dNode = new DCharmNode(nullptr, nullptr, nullptr);
+dNode->insertChild(new DCharmNode(dNode, new DCharmNode::item_set ({1}), new DCharmNode::diff_set ({2,4,5,6}), -2));
+dNode->insertChild(new DCharmNode(dNode, new DCharmNode::item_set ({5}), new DCharmNode::diff_set ({1,2,3,4,5,6}), -2));
+dNode->insertChild(new DCharmNode(dNode, new DCharmNode::item_set ({2}), new DCharmNode::diff_set ({1,3,5,6}), -2));
+dNode->insertChild(new DCharmNode(dNode, new DCharmNode::item_set ({4}), new DCharmNode::diff_set ({1,2,3,4,5}), -2));
+dNode->insertChild(new DCharmNode(dNode, new DCharmNode::item_set ({3}), new DCharmNode::diff_set ({1,3,4,5}), -2));
+
+DCharm dcharm = DCharm();
+dcharm.dcharm(&dNode, 3);
+dcharm.printClosedItemsets();
 return 0;
 }
