29db17c53d5130431c76c72663d4275f19639c6b
ekowalsk <ewcia.kowalska@gmail.com>
2020-01-16 23:17:09 +0100

    poprawiono wywołanie konstuktorów o parametr Mode

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 int main() {
 CharmNode * node = new CharmNode(nullptr, nullptr, nullptr);
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({1}), new CharmNode::tid_list ({2,4,5,6})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({5}), new CharmNode::tid_list ({1,2,3,4,5,6})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({2}), new CharmNode::tid_list ({1,3,5,6})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({4}), new CharmNode::tid_list ({1,2,3,4,5})));
-node->insertChild(new CharmNode(node, new CharmNode::item_set ({3}), new CharmNode::tid_list ({1,3,4,5})));
+    node->insertIChild(new CharmNode(node, new CharmNode::item_set({1}), new CharmNode::tid_list({2, 4, 5, 6})));
+    node->insertIChild(new CharmNode(node, new CharmNode::item_set({5}), new CharmNode::tid_list({1, 2, 3, 4, 5, 6})));
+    node->insertIChild(new CharmNode(node, new CharmNode::item_set({2}), new CharmNode::tid_list({1, 3, 5, 6})));
+    node->insertIChild(new CharmNode(node, new CharmNode::item_set({4}), new CharmNode::tid_list({1, 2, 3, 4, 5})));
+    node->insertIChild(new CharmNode(node, new CharmNode::item_set({3}), new CharmNode::tid_list({1, 3, 4, 5})));
 
 Charm charm = Charm();
 charm.charm(&node, 2);
